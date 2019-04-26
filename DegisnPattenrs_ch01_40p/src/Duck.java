7117268b1bb84c5bf2aeaf7f8056957527b73c40
dogyu <dydqud@gmail.com>
2019-04-27 03:14:19 +0900

    40page all

---
+++ DegisnPattenrs_ch01_40p/src/Duck.java
@@ -0,0 +1,16 @@
+// 오리 공통
+public class Duck {
+	 
+	public void quack() {
+		System.out.println("꽥 거리다");
+	};
+	
+	public void swim() {
+		System.out.println("수영하고 있다");		
+		
+	};
+	
+	public void display() {
+		System.out.println("오리가 화면에 표시 되다");
+	};
+}
